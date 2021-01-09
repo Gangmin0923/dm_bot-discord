57d0a14e38e55e2a28be946f6cf4170b07bc6940
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2021-01-09 13:51:37 +0900

    Update dmmm.py

--- dmmm.py
+++ dmmm.py
@@ -23,13 +23,12 @@ async def on_message(message):
             embed = discord.Embed(colour=discord.Colour.blue(), timestamp=message.created_at)
             embed.add_field(name='전송자', value=message.author, inline=False)
             embed.add_field(name='내용', value=message.content, inline=False)
-            embed.set_footer(text=f'!디엠 {message.author.id} [할말] 을 통해 답장을 보내주세요!')
+            embed.set_footer(text=f'!디엠 <@{message.author.id}> [할말] 을 통해 답장을 보내주세요!')
             await client.get_channel(channel).send(f"`{message.author.name}({message.author.id})`", embed=embed)
 
     if message.content.startswith('!디엠'):
-        author = client.get_user(int(message.content[4:22]))
-        msg = message.content[23:]
-        await author.send(f"**{message.author.name}** 님의 답장: {msg}")
-        await message.channel.send(f'`{author}`에게 DM을 보냈습니다')
+        msg = message.content[25:]
+        await message.mentions[0].send(f"**{message.author.name}** 님의 답장: {msg}")
+        await message.channel.send(f'`{message.mentions[0]}`에게 DM을 보냈습니다')
         
 client.run(token)
