4244d6ad058e41bdcf38eab858fc6bfedf5c7f80
Gangmin0923 <77842905+Gangmin0923@users.noreply.github.com>
2021-01-23 00:35:04 +0900

    Update dmmm.py

--- dmmm.py
+++ dmmm.py
@@ -3,13 +3,13 @@ import discord
 client = discord.Client()
 
 
-token = '★봇토큰★'
+token = 'Nzk5MzA0MzM4MDk0MDMwODQ5.YABoLA.Me2pv6pE0jfptBbMwejBq4igpQQ'
 
 
 @client.event
 async def on_ready():
     print("봇이 성공적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('DM 항시가능')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
@@ -23,7 +23,7 @@ async def on_message(message):
             embed.add_field(name='전송자', value=message.author, inline=False)
             embed.add_field(name='내용', value=message.content, inline=False)
             embed.set_footer(text=f'!디엠 <@{message.author.id}> [할말] 을 통해 답장을 보내주세요!')
-            await client.get_channel(★디엠 내용 보내질 채널ID★).send(f"`{message.author.name}({message.author.id})`", embed=embed)
+            await client.get_channel(802199509022801940).send(f"`{message.author.name}({message.author.id})`", embed=embed)
 
     if message.content.startswith('!디엠'):
         if message.author.guild_permissions.manage_messages:
